bd2ef19aa7252956b6da5cd3e3605d72ca316990
Sharir Shazni Sazali <shrrshazni@gmail.com>
2023-10-15 00:30:14 +0800

    Level 3 - Hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,10 +26,12 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
+//mongoose encryption
+
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -46,7 +49,7 @@ app.get("/login", function (req, res) {
 
 app.post("/login", async function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   const checkUser = await User.findOne({ name: username });
 
@@ -70,7 +73,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     name: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   const checkResult = newUser.save();
